79d537dba2c140c4bbdb0fe4429fc94384eab70a
RileyMoore01 <xrileyxmoorex@outlook.com>
2023-11-21 15:24:49 -0600

    side menu

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 window.onload = () => {
   const form1 = document.querySelector("#addForm");
-
-  let
 }
+
+
+
+function openNav() {
+  document.getElementById("nav-bar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("nav-bar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "black";
+} 
